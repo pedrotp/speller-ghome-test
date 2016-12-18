df42d1838cedeaafaa461540fe4d393387864309
pedrotp <pedro@pedrotp.com>
2016-12-17 23:25:40 -0500

    basic func done

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+// var ApiAi = require('actions-on-google').ApiAiAssistant;
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+// express middleware setup
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+  var arr = req.result.parameters.letter;
+  var word = "";
+  for (var i = 0; i < arr.length; i++) {
+    word += arr[i];
+  }
+  res.status(200).json({
+    "speech": word,
+    "source": "apiai-speller",
+    "displayText": word
+  });
+});
+
+app.listen(port, function () {
+  console.log('SoulScraper app listening on port ' + port + '!');
+});
